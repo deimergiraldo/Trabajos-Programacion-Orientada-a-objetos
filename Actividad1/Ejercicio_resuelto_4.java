2024c90ffeebc566304babd1ba29a34690cb2a13
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-02-28 12:04:59 -0500

    Create Ejercicio_resuelto_4.java

---
+++ Actividad1/Ejercicio_resuelto_4.java
@@ -0,0 +1,15 @@
+package com.mycompany.ejercicio_resuelto_4;
+
+public class Ejercicio_resuelto_4 {
+
+    public static void main(String[] args) {
+        int EDJUAN=9;
+        int EDALBER,EDANA,EDMAMA;
+        EDALBER= (2)*(EDJUAN/3);
+        EDANA= (4)*(EDJUAN/3);
+        EDMAMA=EDJUAN + EDALBER + EDANA;
+        System.out.println("Las edades son");
+        System.out.println("ALBERTO:"+ EDALBER + "  JUAN:"+ EDJUAN);
+        System.out.println("ANA:"+ EDANA + "  MAMA:" +EDMAMA);
+    }
+}
