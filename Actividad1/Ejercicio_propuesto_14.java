8de99298f4c0dd5c2ec82acb7cb9c30e6d5d6ece
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-02-28 12:07:45 -0500

    Create Ejercicio_propuesto_14.java

---
+++ Actividad1/Ejercicio_propuesto_14.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejercicio_propuesto_14;
+
+public class Ejercicio_propuesto_14 {
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero=3;
+        cuadrado= Math.pow(numero, 2);
+        cubo= Math.pow(numero, 3);
+        System.out.println("El número es: " + numero);
+        System.out.println("El número al cuadrado es: " + cuadrado);
+        System.out.println("El número al cubo es: " + cubo);
+    }
+}
