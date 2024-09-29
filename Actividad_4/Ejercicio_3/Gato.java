77be20a206c926898e8031646de8c1af87067e50
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-09-28 22:02:05 -0500

    Create Gato.java

---
+++ Actividad_4/Ejercicio_3/Gato.java
@@ -0,0 +1,18 @@
+package Animales;
+
+
+public class Gato extends Felino{
+    public String getSonido() {
+        return "Maullido";
+    }
+    public String getAlimentos() {
+        return "Ratones";
+    }
+    public String getHabitat() {
+        return "Doméstico";
+    }
+    public String getNombreCientifico() {
+        return "Felis silvestris catus";
+    }
+    
+}
