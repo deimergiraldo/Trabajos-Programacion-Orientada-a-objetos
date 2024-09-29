48f3c4801643d56e8b360687bf6eabeb10d61f58
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-09-28 21:59:22 -0500

    Create Perro.java

---
+++ Actividad_4/Ejercicio_3/Perro.java
@@ -0,0 +1,17 @@
+package Animales;
+
+public class Perro extends Canino{
+    public String getSonido() {
+        return "Ladrido";
+    }
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+    public String getHabitat() {
+        return "Doméstico";
+    }
+    public String getNombreCientifico() {
+        return "Canis lupus familiaris";
+    }
+    
+}
