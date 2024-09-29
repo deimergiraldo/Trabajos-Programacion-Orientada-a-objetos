6ab5a07c1c3c7ae39eff2a94e49ab6790fad1532
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-09-28 22:01:31 -0500

    Create Lobo.java

---
+++ Actividad_4/Ejercicio_3/Lobo.java
@@ -0,0 +1,17 @@
+package Animales;
+
+public class Lobo extends Canino{
+    public String getSonido() {
+        return "Aullido";
+    }
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+    public String getHabitat() {
+        return "Bosque";
+    }
+    public String getNombreCientifico() {
+        return "Canis lupus";
+    }
+    
+}
