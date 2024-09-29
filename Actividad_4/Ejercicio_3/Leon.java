d90ac1581f3ae0386b0d41532dbe6e24243ef20f
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-09-28 22:02:34 -0500

    Create Leon.java

---
+++ Actividad_4/Ejercicio_3/Leon.java
@@ -0,0 +1,17 @@
+package Animales;
+
+
+public class Leon extends Felino {
+    public String getSonido() {
+        return "Rugido";
+    }
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+    public String getHabitat() {
+        return "Praderas";
+    }
+    public String getNombreCientifico() {
+        return "Panthera leo";
+    }
+}
