bdda2ccf9de479cb65e5204b66b8303a1269ac16
deimergiraldo <81828328+deimergiraldo@users.noreply.github.com>
2024-09-30 22:17:21 -0500

    Create Principal.java

---
+++ Actividad_4/Ejercicio_5/Principal.java
@@ -0,0 +1,14 @@
+package com.mycompany.principal;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+    }
+}
